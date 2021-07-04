a2608f8463d23ecc0399fff29a46e918ce6378cc
horik <horik@syneforge.com>
2021-07-05 00:07:26 +0300

    qwe

---
+++ src/test/java/Test123.java
@@ -0,0 +1,11 @@
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Test123 {
+    @Test
+    public void qwe() {
+        assertThat("test", 2 + 2, Matchers.equalTo(5));
+    }
+}
