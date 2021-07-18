9e74e7738679b3fec1f366599d9cd29ddecbf7b6
horik <horik@syneforge.com>
2021-07-18 18:19:13 +0300

    qwe

--- src/test/java/Test123.java
+++ src/test/java/Test123.java
@@ -1,8 +1,10 @@
+import io.qameta.allure.Feature;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@Feature("qweqwasd")
 public class Test123 {
     @Test
     public void qwe() {
