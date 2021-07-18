ef23561327f336c35ac58c16ac86175bf66421de
horik <horik@syneforge.com>
2021-07-18 22:08:34 +0300

    qwe

--- src/main/java/Steps.java
+++ src/main/java/Steps.java
@@ -7,11 +7,17 @@ public class Steps {
     @Step("hohoho {0}, {1}")
     public static void plus(int i, int b) {
         int c = i + b;
+        substep123();
         substep(c);
     }
 
     @Step("su step")
     public static void substep(int i) {
         assertThat("test", i, Matchers.equalTo(42));
+    }
+
+    @Step("su step")
+    public static void substep123() {
+        assertThat("test", 42, Matchers.equalTo(42));
     }
 }
