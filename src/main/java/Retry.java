b4160e19b514b6b16d3581a681ca6e7797dc8b3c
horik <horik@syneforge.com>
2021-07-18 21:56:09 +0300

    qwe

--- src/main/java/Retry.java
+++ src/main/java/Retry.java
@@ -1,18 +1,10 @@
 import org.testng.IAnnotationTransformer;
-import org.testng.IRetryAnalyzer;
-import org.testng.ITestResult;
 import org.testng.annotations.ITestAnnotation;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class Retry implements IAnnotationTransformer {
-
-    public void disabler(Method method, ITestResult result) {
-        if (method.getName().equals("qwe")) {
-            result.setStatus(ITestResult.SKIP);
-        }
-    }
 
     @Override
     public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
