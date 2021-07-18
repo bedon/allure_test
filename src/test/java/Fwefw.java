c29b2c29119173fabff2135dd0a1b18f47cf259f
horik <horik@syneforge.com>
2021-07-18 18:25:29 +0300

    qwe

---
+++ src/test/java/Fwefw.java
@@ -0,0 +1,36 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@Feature("qweqwasd")
+@Story("sfbfgnfg")
+public class Fwefw {
+
+    @Test
+    public void qwe() {
+        assertThat("test", 2 + 2, Matchers.equalTo(42));
+    }
+
+    @Test
+    public void qwdqwd() {
+        assertThat("test", 2 + 2, Matchers.equalTo(4));
+    }
+
+    @Test
+    public void qwsffnghnfe() {
+        assertThat("test", 2 + 2, Matchers.equalTo(4));
+    }
+
+    @Test
+    public void fsdvsdv() {
+        assertThat("test", 2 + 2, Matchers.equalTo(42));
+    }
+
+    @Test
+    public void fgrth45ge() {
+        assertThat("test", 2 + 2, Matchers.equalTo(4));
+    }
+}
